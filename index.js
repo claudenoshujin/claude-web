75982d8167ca7e073ab0df9c2941f6fd1fe37578
lulu <claudialu0914@gmail.com>
2026-08-01 06:01:40 -0400

    Keep mobile icon strokes crisp

--- index.js
+++ index.js
@@ -251,7 +251,7 @@ const CLAUDE_KEYBOARD_BUILD = {
      只改 CSS 内容、不改这个字符串，用户端（尤其 TauriTavern 这类会长期
      缓存磁盘资源的原生壳）拉到的还是旧样式表，看起来像"更新了但没修复"。
      以后只要改了 styles/*.css，这里必须跟着换一个新值。 */
-  id: '2.0.28-mobile-cropped-icons-background-drawer-' + CLAUDE_THEME_VARIANT + '-' + CLAUDE_LAYOUT + '-ext',
+  id: '2.0.29-mobile-crisp-strokes-' + CLAUDE_THEME_VARIANT + '-' + CLAUDE_LAYOUT + '-ext',
   mode: 'full',
 };
 
